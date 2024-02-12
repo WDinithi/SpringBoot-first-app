fe5260bada474fe2f91ad3e8e174971a481d9708
WDinithi <wijewardanadinithi@gmail.com>
2024-02-12 16:38:35 +0530

    Employee update coding

--- src/main/java/com/example/springbootapp/Controller/EmployeeController.java
+++ src/main/java/com/example/springbootapp/Controller/EmployeeController.java
@@ -7,10 +7,7 @@ import com.example.springbootapp.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/employee")
@@ -32,7 +29,40 @@ public class EmployeeController {
                 responseDTO.setContact(employeeDTO);
                 return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
             }
-            else if (res.equals("006")){
+            else if (res.equals("06")){
+                responseDTO.setCode(VarList.RSP_DUPLICATED);
+                responseDTO.setMassage("Employee registered");
+                responseDTO.setContact(employeeDTO);
+                return new ResponseEntity(responseDTO,HttpStatus.BAD_REQUEST);
+
+            }
+            else {
+                responseDTO.setCode(VarList.RSP_FAIL);
+                responseDTO.setMassage("Error");
+                responseDTO.setContact(null);
+                return new ResponseEntity(responseDTO,HttpStatus.BAD_REQUEST);
+            }
+
+        }
+        catch (Exception e){
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMassage("Error");
+            responseDTO.setContact(null);
+            return new ResponseEntity(responseDTO,HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/UpdateEmployee")
+    public ResponseEntity updateEmployee(@RequestBody EmployeeDTO employeeDTO){
+        try {
+            String res=employeeService.saveEmployee(employeeDTO);
+            if (res.equals("00")){
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMassage(VarList.RSP_SUCCESS);
+                responseDTO.setContact(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+            }
+            else if (res.equals("01")){
                 responseDTO.setCode(VarList.RSP_DUPLICATED);
                 responseDTO.setMassage("Employee registered");
                 responseDTO.setContact(employeeDTO);
